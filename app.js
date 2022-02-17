15d54fa92b6d682cff218ca70c1274dff7f2acd8
Scalaptia <fernando@haroware.com>
2022-02-16 19:26:13 -0800

    Make cell size relative to row length

--- app.js
+++ app.js
@@ -1,27 +1,32 @@
 const grid = document.getElementById('grid');
-let num = 16;
+let length = 25;
+let cellSize = grid.clientWidth / length;
+
+genDivs(length);
 
 // Create grid of divs
-function genDivs(num){ 
-    for(i = 0; i < num; i++){ 
+function genDivs(length){ 
+    for(i = 0; i < length; i++){ 
       let row = document.createElement("div"); 
       row.className = "row"; 
-      for(x = 1; x <= num; x++){ 
+      for(x = 1; x <= length; x++){ 
           let cell = document.createElement("div"); 
-          cell.className = "gridsquare"; 
+          cell.className = "gridsquare";
+          cell.style.width = `${cellSize}px`;
+          cell.style.height = `${cellSize}px`;
           row.appendChild(cell); 
       } 
       grid.appendChild(row);
     } 
 }
-genDivs(num);
+
 
 // Delete grid divs and create new div
-function clearGrid (num) {
+function clearGrid (length) {
     while (grid.firstChild) {
         grid.removeChild(grid.lastChild);
       }
-      genDivs(num);
+      genDivs(length);
     }
 
     
@@ -31,3 +36,5 @@ grid.addEventListener('mouseover', function (e) {
       e.target.classList.add('gridhover');
     }
   });
+
+
